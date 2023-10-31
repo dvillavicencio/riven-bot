f1c050c46bafdf8fa2afa9b2f5f41b3c1fe98f4c
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2023-10-30 23:09:16 -0700

    Add wildcard character to ResponseEntity

--- src/main/java/com/danielvm/destiny2bot/controller/InteractionsController.java
+++ src/main/java/com/danielvm/destiny2bot/controller/InteractionsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class InteractionsController {
 
     @PostMapping(value = "/interactions")
-    public ResponseEntity<> getInteractions() {
+    public ResponseEntity<?> getInteractions() {
         return null;
     }
 }
