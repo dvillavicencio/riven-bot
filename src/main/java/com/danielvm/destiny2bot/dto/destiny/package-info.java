40637738aa9283a7d04d5926607be189ed8eca1a
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2023-10-30 23:06:35 -0700

    Add package-info + add empty-non-functionaing interactions API

--- src/main/java/com/danielvm/destiny2bot/dto/destiny/package-info.java
+++ src/main/java/com/danielvm/destiny2bot/dto/destiny/package-info.java
@@ -1,8 +1,8 @@
 /**
  * This package has stuff that comes directly from Bungie's API
  * <p>
- * Nothing here is intended to be returned from any REST Api before actually mapping it,
- * since most Bungie API response definitions use hashes and their manifest database.
+ * Nothing here is intended to be returned from any REST Api before actually mapping it
+ * since most Bungie-API-response-definitions use hashes and the manifest database.
  * </p>
  */
 package com.danielvm.destiny2bot.dto.destiny;
