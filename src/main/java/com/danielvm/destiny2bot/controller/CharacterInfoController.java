8aee4c1b0f4f01fa4198131d68cb096931094a86
Daniel Villavicencio <48806616+dvillavicencio@users.noreply.github.com>
2023-11-05 00:34:14 -0700

    Added Java 21 lombok dependency fixes + created local-yml for Redis (#9)
    * Added Java 21 lombok dependency fixes + created local-yml for Redis
    
    * Upgrade pipeline to run Java 21
    
    * Re-add Java distribution
    
    ---------
    
    Co-authored-by: Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>

--- src/main/java/com/danielvm/destiny2bot/controller/CharacterInfoController.java
+++ src/main/java/com/danielvm/destiny2bot/controller/CharacterInfoController.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@ResponseBody
 public class CharacterInfoController {
 
     private final CharacterInfoService characterInfoService;
