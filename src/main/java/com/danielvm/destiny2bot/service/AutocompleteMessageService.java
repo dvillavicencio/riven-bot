abb652c3f8cb84e50cd06a1259f93718cfaa3015
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2023-12-26 02:47:25 -0800

    Start working on adding autocomplete for raid_stats and fix integration test bug

---
+++ src/main/java/com/danielvm/destiny2bot/service/AutocompleteMessageService.java
@@ -0,0 +1,9 @@
+package com.danielvm.destiny2bot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutocompleteMessageService {
+
+
+}
--- src/main/java/com/danielvm/destiny2bot/service/InteractionService.java
+++ src/main/java/com/danielvm/destiny2bot/service/InteractionService.java
@@ -1,11 +1,14 @@
 package com.danielvm.destiny2bot.service;
 
-import static com.danielvm.destiny2bot.enums.InteractionType.PING;
-import static com.danielvm.destiny2bot.enums.InteractionType.findByValue;
+import static com.danielvm.destiny2bot.enums.InteractionType.APPLICATION_COMMAND;
 
+import com.danielvm.destiny2bot.dto.discord.Choice;
 import com.danielvm.destiny2bot.dto.discord.Interaction;
 import com.danielvm.destiny2bot.dto.discord.InteractionResponse;
+import com.danielvm.destiny2bot.dto.discord.InteractionResponseData;
 import com.danielvm.destiny2bot.enums.CommandEnum;
+import com.danielvm.destiny2bot.enums.InteractionType;
+import java.util.List;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,11 +18,14 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class InteractionService {
 
-  private final MessageRegistryService commandFactoryService;
+  private final MessageRegistryService messageRegistryService;
+  private final AutocompleteMessageService autocompleteMessageService;
 
   public InteractionService(
+      AutocompleteMessageService autocompleteMessageService,
       MessageRegistryService messageRegistryService) {
-    this.commandFactoryService = messageRegistryService;
+    this.messageRegistryService = messageRegistryService;
+    this.autocompleteMessageService = autocompleteMessageService;
   }
 
   /**
@@ -30,11 +36,19 @@ public class InteractionService {
    * @return {@link InteractionResponse}
    */
   public Mono<InteractionResponse> handleInteraction(Interaction interaction) {
-    var interactionType = findByValue(interaction.getType());
-    boolean notPingRequest = !Objects.equals(interactionType, PING);
-    if (notPingRequest) {
+    var interactionType = InteractionType.findByValue(interaction.getType());
+    boolean isApplicationCommand = Objects.equals(interactionType, APPLICATION_COMMAND);
+    boolean isAutocomplete = Objects.equals(interactionType,
+        InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE);
+    if (isAutocomplete) {
+      return Mono.just(
+          new InteractionResponse(8,
+              InteractionResponseData.builder()
+                  .choices(List.of(new Choice("Titan Level 1800", "Titan Level 1800"),
+                      new Choice("Titan Level 1800", "Titan Level 1800"))).build()));
+    } else if (isApplicationCommand) {
       var slashCommand = CommandEnum.findByName(interaction.getData().getName());
-      return commandFactoryService.getFactory(slashCommand).createResponse();
+      return messageRegistryService.getFactory(slashCommand).createResponse();
     } else {
       return Mono.just(InteractionResponse.PING());
     }
