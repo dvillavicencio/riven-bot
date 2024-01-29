83f913622b826c10468b004329f5c2232e1608a2
Daniel Villavicencio <48806616+dvillavicencio@users.noreply.github.com>
2024-01-29 11:33:56 -0800

    Create flyway migration and database details (#57)
    * Added postgres, flyway, and testcontainers for testing environment
    
    * Ditched spring data jpa, now using only jdbc
    
    * Add TODOs for production database

---
+++ src/main/java/com/danielvm/destiny2bot/config/FlywayConfiguration.java
@@ -0,0 +1,25 @@
+package com.danielvm.destiny2bot.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({R2dbcProperties.class, FlywayProperties.class})
+public class FlywayConfiguration {
+
+  @Bean(initMethod = "migrate")
+  public Flyway flyway(FlywayProperties flywayProperties, R2dbcProperties r2dbcProperties) {
+    return Flyway.configure()
+        .dataSource(
+            flywayProperties.getUrl(),
+            r2dbcProperties.getUsername(),
+            r2dbcProperties.getPassword())
+        .locations(flywayProperties.getLocations().get(0))
+        .baselineOnMigrate(true)
+        .load();
+  }
+}
