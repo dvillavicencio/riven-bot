452d788fe96d234be1830129016186117992c8b6
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2024-02-20 12:50:05 -0800

    Add collection name to PGCRDetails entity class

--- src/main/java/com/danielvm/destiny2bot/entity/PGCRDetails.java
+++ src/main/java/com/danielvm/destiny2bot/entity/PGCRDetails.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
+@Document(collection = "pgcr_details")
 public class PGCRDetails {
 
   /**
