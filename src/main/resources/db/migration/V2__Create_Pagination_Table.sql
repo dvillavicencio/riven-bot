18fb4f241a554f7e641c9d11d3518ea2c28d84c7
Daniel Villavicencio <danielvillavicencio@Daniels-MBP.attlocal.net>
2024-04-24 02:39:02 -0700

    Small commit to remove the db/migration/** directory

--- src/main/resources/db/migration/V2__Create_Pagination_Table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_page_information
-(
-    user_discord_id BIGINT PRIMARY KEY,
-    number_of_pages INTEGER,
-    last_page_count INTEGER,
-    CONSTRAINT discord_id_fk FOREIGN KEY (user_discord_id) REFERENCES bot_user (discord_id)
-);
